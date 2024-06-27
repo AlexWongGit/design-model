e2d3821a0fd679d3203e96570da4e3bf42cdd589
wangzf <wangzf@ztesa.com.cn>
2024-06-27 13:35:29 +0800

    feat(newcode): 实现了两种方法计算二叉树的最大深度在DepthOfBinaryTree.java中，添加了两种计算二叉树最大深度的方法： 1. 递归深度优先搜索（DFS）方法； 2. 层序遍历方法。
    每种方法都提供了相应的注释说明其工作原理和实现方式。

--- newcode/src/main/java/simple/DepthOfBinaryTree.java
+++ newcode/src/main/java/simple/DepthOfBinaryTree.java
@@ -1,4 +1,84 @@
 package simple;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class DepthOfBinaryTree {
+
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        System.out.println(maxDepth(root));
+    }
+
+    /**
+     * @description DFS
+     * @return int
+     * @param root
+     */
+    private static int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    private static int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    /**
+     * @description 层序遍历
+     * @return int
+     * @param root
+     */
+    private static int sequenceTraversal(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode.left != null) {
+                    queue.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.add(treeNode.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+
 }
