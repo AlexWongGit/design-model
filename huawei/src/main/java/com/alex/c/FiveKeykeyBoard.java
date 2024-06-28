61f88a33ae9fb00e4db83608cf48bd16c71a8e6c
wangzf <wangzf@ztesa.com.cn>
2024-06-28 18:07:13 +0800

    feat(huawei): 添加FiveKeykeyBoard类以解决华为键盘问题
    新建了FiveKeykeyBoard类，包含了一个静态方法print0，用于根据给定的指令数组模拟华为五键键盘的操作，并返回最终形成的字符串长度。该方法实现了根据不同的指令对当前选择的字符长度进行增减或设置，以及对已选择的字符进行附加等操作。
    
    这个提交添加了新的代码，没有修改或删除任何现有代码。

---
+++ huawei/src/main/java/com/alex/c/FiveKeykeyBoard.java
@@ -0,0 +1,54 @@
+package com.alex.c;
+
+public class FiveKeykeyBoard {
+
+    public static void main(String[] args) {
+        int[] command = {1, 1, 1};
+        int res = print0(command);
+        System.out.println(res);
+
+    }
+
+    private static int print0(int[] command) {
+        int attach = 0;
+        int curLength = 0;
+        boolean select = false;
+        for (int i = 0; i < command.length; i++) {
+            switch (command[i]) {
+                case 1:
+                    if (select) {
+                        curLength = 0;
+                    }
+                    curLength++;
+                    select = false;
+                    break;
+                case 2:
+                    if (select) {
+                        attach = curLength;
+                    } else {
+                        attach = 1;
+                    }
+                    break;
+                case 3:
+                    attach = curLength;
+                    curLength = 0;
+                    select = false;
+                    break;
+                case 4:
+                    if (select) {
+                        curLength = 0;
+                    }
+                    curLength += attach;
+                    select = false;
+                    break;
+                case 5:
+                    select = true;
+                    break;
+                default:
+            }
+        }
+        return curLength;
+    }
+
+}
+
